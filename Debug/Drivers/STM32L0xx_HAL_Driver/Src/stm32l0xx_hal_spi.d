8aea10b261732c9ff99092904f92e3e2dc37e331
Usagitoneko <hogouxian@gmail.com>
2017-07-18 20:37:38 +0800

    working with 1 byte (by checking ss pin)

---
+++ Debug/Drivers/STM32L0xx_HAL_Driver/Src/stm32l0xx_hal_spi.d
@@ -0,0 +1,80 @@
+Drivers/STM32L0xx_HAL_Driver/Src/stm32l0xx_hal_spi.o: \
+ ../Drivers/STM32L0xx_HAL_Driver/Src/stm32l0xx_hal_spi.c \
+ C:/Students/HGX/stm32/spi_slave/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal.h \
+ C:/Students/HGX/stm32/spi_slave/Inc/stm32l0xx_hal_conf.h \
+ C:/Students/HGX/stm32/spi_slave/Inc/main.h \
+ C:/Students/HGX/stm32/spi_slave/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_rcc.h \
+ C:/Students/HGX/stm32/spi_slave/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_def.h \
+ C:/Students/HGX/stm32/spi_slave/Drivers/CMSIS/Device/ST/STM32L0xx/Include/stm32l0xx.h \
+ C:/Students/HGX/stm32/spi_slave/Drivers/CMSIS/Device/ST/STM32L0xx/Include/stm32l053xx.h \
+ C:/Students/HGX/stm32/spi_slave/Drivers/CMSIS/Include/core_cm0plus.h \
+ C:/Students/HGX/stm32/spi_slave/Drivers/CMSIS/Include/core_cmInstr.h \
+ C:/Students/HGX/stm32/spi_slave/Drivers/CMSIS/Include/cmsis_gcc.h \
+ C:/Students/HGX/stm32/spi_slave/Drivers/CMSIS/Include/core_cmFunc.h \
+ C:/Students/HGX/stm32/spi_slave/Drivers/CMSIS/Device/ST/STM32L0xx/Include/system_stm32l0xx.h \
+ C:/Students/HGX/stm32/spi_slave/Drivers/STM32L0xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ C:/Students/HGX/stm32/spi_slave/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_rcc_ex.h \
+ C:/Students/HGX/stm32/spi_slave/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_gpio.h \
+ C:/Students/HGX/stm32/spi_slave/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_gpio_ex.h \
+ C:/Students/HGX/stm32/spi_slave/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_dma.h \
+ C:/Students/HGX/stm32/spi_slave/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_cortex.h \
+ C:/Students/HGX/stm32/spi_slave/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_flash.h \
+ C:/Students/HGX/stm32/spi_slave/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_flash_ex.h \
+ C:/Students/HGX/stm32/spi_slave/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_flash_ramfunc.h \
+ C:/Students/HGX/stm32/spi_slave/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_i2c.h \
+ C:/Students/HGX/stm32/spi_slave/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_i2c_ex.h \
+ C:/Students/HGX/stm32/spi_slave/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_pwr.h \
+ C:/Students/HGX/stm32/spi_slave/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_pwr_ex.h \
+ C:/Students/HGX/stm32/spi_slave/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_spi.h
+
+C:/Students/HGX/stm32/spi_slave/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal.h:
+
+C:/Students/HGX/stm32/spi_slave/Inc/stm32l0xx_hal_conf.h:
+
+C:/Students/HGX/stm32/spi_slave/Inc/main.h:
+
+C:/Students/HGX/stm32/spi_slave/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_rcc.h:
+
+C:/Students/HGX/stm32/spi_slave/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_def.h:
+
+C:/Students/HGX/stm32/spi_slave/Drivers/CMSIS/Device/ST/STM32L0xx/Include/stm32l0xx.h:
+
+C:/Students/HGX/stm32/spi_slave/Drivers/CMSIS/Device/ST/STM32L0xx/Include/stm32l053xx.h:
+
+C:/Students/HGX/stm32/spi_slave/Drivers/CMSIS/Include/core_cm0plus.h:
+
+C:/Students/HGX/stm32/spi_slave/Drivers/CMSIS/Include/core_cmInstr.h:
+
+C:/Students/HGX/stm32/spi_slave/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+C:/Students/HGX/stm32/spi_slave/Drivers/CMSIS/Include/core_cmFunc.h:
+
+C:/Students/HGX/stm32/spi_slave/Drivers/CMSIS/Device/ST/STM32L0xx/Include/system_stm32l0xx.h:
+
+C:/Students/HGX/stm32/spi_slave/Drivers/STM32L0xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+C:/Students/HGX/stm32/spi_slave/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_rcc_ex.h:
+
+C:/Students/HGX/stm32/spi_slave/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_gpio.h:
+
+C:/Students/HGX/stm32/spi_slave/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_gpio_ex.h:
+
+C:/Students/HGX/stm32/spi_slave/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_dma.h:
+
+C:/Students/HGX/stm32/spi_slave/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_cortex.h:
+
+C:/Students/HGX/stm32/spi_slave/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_flash.h:
+
+C:/Students/HGX/stm32/spi_slave/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_flash_ex.h:
+
+C:/Students/HGX/stm32/spi_slave/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_flash_ramfunc.h:
+
+C:/Students/HGX/stm32/spi_slave/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_i2c.h:
+
+C:/Students/HGX/stm32/spi_slave/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_i2c_ex.h:
+
+C:/Students/HGX/stm32/spi_slave/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_pwr.h:
+
+C:/Students/HGX/stm32/spi_slave/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_pwr_ex.h:
+
+C:/Students/HGX/stm32/spi_slave/Drivers/STM32L0xx_HAL_Driver/Inc/stm32l0xx_hal_spi.h:
